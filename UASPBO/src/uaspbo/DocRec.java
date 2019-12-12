35f4eae8de970d2b86e450a43fe90b48c5dc4bbe
KharisMm19 <kharismm19@gmail.com>
2019-12-12 16:49:32 +0700

    DocRec

--- UASPBO/src/uaspbo/DocRec.java
+++ UASPBO/src/uaspbo/DocRec.java
@@ -5,17 +5,39 @@
  */
 package uaspbo;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+import net.proteanit.sql.DbUtils;
+
 /**
  *
  * @author VENTI
  */
 public class DocRec extends javax.swing.JFrame {
 
+    Connection con = null;
+    ResultSet rs = null;
+    PreparedStatement pst = null;
+
     /**
      * Creates new form DokterForm
      */
     public DocRec() {
         initComponents();
+        con= DBHelper.ConnectDB();
+        Get_Data();
+    }
+    
+    private void Get_Data(){
+      String sql="select DoctorID as 'Doctor ID', DoctorName as 'Doctor Name',FatherName as 'Father Name',Address,ContacNo as 'Contact No',Email as 'Email ID',Qualifications,Gender,BloodGroup as 'Blood Group',DateOfJoining as 'Joining Date' from Doctor order by DoctorName";        
+      try{
+         pst=con.prepareStatement(sql);
+          rs= pst.executeQuery();
+         jTable1.setModel(DbUtils.resultSetToTableModel(rs));
+         }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+          
+        }
     }
 
     /**
@@ -27,21 +49,84 @@ public class DocRec extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        try {
+            con = DBHelper.ConnectDB();
+            int row = jTable1.getSelectedRow();
+            String table_click = jTable1.getModel().getValueAt(row, 0).toString();
+            String sql = "select * from Doctor where DoctorID = '" + table_click + "'";
+            pst = con.prepareStatement(sql);
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                this.hide();
+                Entry frm = new Entry();
+                frm.setVisible(true);
+                String add1 = rs.getString("DoctorID");
+                frm.txtId.setText(add1);
+                String add2 = rs.getString("Doctorname");
+                frm.txtName.setText(add2);
+                String add3 = rs.getString("Fathername");
+                frm.txtFname.setText(add3);
+                String add5 = rs.getString("Email");
+                frm.txtE.setText(add5);
+                String add6 = rs.getString("Qualifications");
+                frm.txtQ.setText(add6);
+                String add9 = rs.getString("BloodGroup");
+                frm.cmbB.setSelectedItem(add9);
+                String add11 = rs.getString("Gender");
+                frm.cmbG.setSelectedItem(add11);
+                String add14 = rs.getString("DateOfJoining");
+                frm.txtD.setText(add14);
+                String add15 = rs.getString("Address");
+                frm.txtAd.setText(add15);
+                String add16 = rs.getString("ContacNo");
+                frm.txtC.setText(add16);
+                frm.btnUpdate.setEnabled(true);
+                frm.btnDelete.setEnabled(true);
+                frm.btnSave.setEnabled(false);
+
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, ex);
+        }        // TODO add your handling code here:
+    }//GEN-LAST:event_jTable1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -80,5 +165,7 @@ public class DocRec extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
